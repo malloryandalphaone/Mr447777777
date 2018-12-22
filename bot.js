ac28acdff548ade3e1b3d229da5bb93720489ded
malloryandalphaone <42677997+malloryandalphaone@users.noreply.github.com>
2018-12-22 03:07:31 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 const client2 = new Discord.Client();
 
 client.on('ready', () => {
-client.user.setGame("bradh", "WATCHING");
+client.user.setGame("bradh");
    console.log(`Ready!`);
 });
 
